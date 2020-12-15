fc0c35deeed5931170bd767de0082e9a7011c127
Giovanni Minniti <minniti@t-online.de>
2020-12-15 11:21:21 +0100

    Get grav version string

---
+++ grav_getver.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# #### #
+# VARS #
+# #### #
+ARGC=$#
+ARGV=("$@")
+RC=0
+
+# #### #
+# LIBS #
+# #### #
+source ${PWD}/libgrav
+
+# ##### #
+# FUNCS #
+# ##### #
+main() {
+   local _ARGC=${1}
+   local _ARGV=("${@}")
+   
+   local _RC=0
+   local _CMD=$(basename ${0})
+
+   local _GRAV_VER="${_ARGV[1]}"
+
+   local _GRAV_TEXT="FAIL: Arguments are not provided!"
+   local _GRAV_ARGS="ARGS: ${_CMD} grav_version"
+   local _GRAV_NOTE="NOTE: (*) are default values, (#) are recommended values"
+   local _GRAV_ARG1="ARG1: grav_version: prod|dev - (#=prod)"
+   local _GRAV_INFO="INFO: ${_CMD} prod"
+
+   if [ ${_ARGC} -lt 1 ]; then usage 1 "${_GRAV_TEXT}" "${_GRAV_ARGS}" "${_GRAV_NOTE}" "${_GRAV_INFO}" "${_GRAV_ARG1}"; fi
+   
+   getver "${_GRAV_VER}"
+
+   _RC=$?
+   
+   return ${_RC}
+}
+
+# #### #
+# MAIN #
+# #### #
+main ${ARGC} "${ARGV[@]}"
+
+RC=$?
+
+# #### #
+# EXIT #
+# #### #
+exit ${RC}
