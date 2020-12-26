4de1f1700cb7cb7b89eddaa5f0210271bdd0ec94
Giovanni Minniti <minniti@t-online.de>
2020-12-26 16:10:28 +0100

    Add URL variable

--- docker/100-runlet-ssl-installation.sh
+++ docker/100-runlet-ssl-installation.sh
@@ -5,8 +5,9 @@ source "${STAGE_DIR}/runlet-head.sh"
 # DO NOT REMOVE ANYTHING BEFORE THIS LINE!
 
 echo "Downloading dehydrated ${DEHYDRATED_VER} package..."
-wget -q --show-progress -O /tmp/dehydrated.tgz \
-     https://github.com/dehydrated-io/dehydrated/releases/download/v${DEHYDRATED_VER}/dehydrated-${DEHYDRATED_VER}.tar.gz
+test -f /tmp/${DEHYDRATED_FILE} || wget -q --show-progress --progress=bar:force \
+     -O /tmp/dehydrated.tgz \
+${DEHYDRATED_URL}/v${DEHYDRATED_VER}/dehydrated-${DEHYDRATED_VER}.tar.gz
 tar -xvzf /tmp/dehydrated.tgz -C /tmp
 cp /tmp/dehydrated-${DEHYDRATED_VER}/dehydrated /usr/local/bin
 
