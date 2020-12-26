b824c44bd55f19f0e3ab94072d9143d4abb4075a
Giovanni Minniti <minniti@t-online.de>
2020-12-26 16:12:57 +0100

    Fix wget progress bar

--- docker/080-runlet-grav-installation.sh
+++ docker/080-runlet-grav-installation.sh
@@ -5,7 +5,7 @@ source "${STAGE_DIR}/runlet-head.sh"
 # DO NOT REMOVE ANYTHING BEFORE THIS LINE!
 
 echo "Installing ${GRAV_URLFILE}...";
-test -f "/tmp/grav/${GRAV_KIND}/${GRAV_URLFILE}" || wget -q --show-progress \
+test -f "/tmp/grav/${GRAV_KIND}/${GRAV_URLFILE}" || wget -q --show-progress --progress=bar:force \
     -O "/tmp/grav/${GRAV_KIND}/${GRAV_URLFILE}" \
     ${GRAV_URL}
 cd /var/www
@@ -16,8 +16,6 @@ ln -s /var/www/${GRAV_NAME} /var/www/html
 echo "Exporting ${GRAV_NAME} path to users..."
 echo "export PATH=/var/www/html/bin:${PATH}" >> /home/${GRAV_USER}/.bashrc
 echo "export PATH=/var/www/html/bin:${PATH}" >> /root/.bashrc
-echo "Create cron job for Grav maintenance scripts for www-data..."
-echo "* * * * * cd /var/www/${GRAV_NAME}; /usr/local/bin/php bin/grav scheduler 1>>/dev/null 2>&1" | crontab -u www-data -
 
 # DO NOT REMOVE ANYTHING BELOW THIS LINE!
 source "${STAGE_DIR}/runlet-tail.sh"
