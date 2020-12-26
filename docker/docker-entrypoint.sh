155a4f6e1a66a93409aea55a0e71a7c9621651e9
Giovanni Minniti <minniti@t-online.de>
2020-12-26 16:11:59 +0100

    Remove dropbear if it is running

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,11 +4,24 @@ set -euo pipefail
 declare _RC=0
 
 declare _GRAV_CMD="${1:-"init"}"
+      
+if [ "${_GRAV_CMD}" == "debug" ]; then set -x; fi      
+
+if [ $(pgrep dropbear | wc -l ) -ge 1 ]; then 
+     kill -9 $(pidof dropbear) 
+fi
 
 case "${_GRAV_CMD}" in
-"init") /usr/sbin/sshd && cron && apache2-foreground; ;;
-"bash") bash; ;;
-     *) exec "$@"; ;;
+   "init"|"debug")
+      su-exec ${GRAV_USER} dropbear -R -w -g -s -a
+      su-exec ${GRAV_USER} gocrond --allow-unprivileged --default-user=${GRAV_USER} grav:/home/${GRAV_USER}/crontab --verbose 1>/tmp/gocrond.log 2>&1 &
+      # Last entry must be apache2 in foreground mode
+      su-exec ${GRAV_USER} apache2-foreground 
+      ;;
+
+   "bash") bash; ;;
+
+   *) exec "$@"; ;;
 esac
 
 exit ${_RC}
