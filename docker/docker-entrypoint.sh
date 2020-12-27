74ecf7e1f25999c1f86d89ddee007b299c0066dc
Giovanni Minniti <minniti@t-online.de>
2020-12-27 21:46:24 +0100

    Remove verbose flag

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -14,7 +14,7 @@ fi
 case "${_GRAV_CMD}" in
    "init"|"debug")
       su-exec ${GRAV_USER} dropbear -R -w -g -s -a
-      su-exec ${GRAV_USER} gocrond --allow-unprivileged --default-user=${GRAV_USER} grav:/home/${GRAV_USER}/crontab --verbose 1>/tmp/gocrond.log 2>&1 &
+      su-exec ${GRAV_USER} gocrond --allow-unprivileged --default-user=${GRAV_USER} grav:/home/${GRAV_USER}/crontab 1>/tmp/gocrond.log 2>&1 &
       # Last entry must be apache2 in foreground mode
       su-exec ${GRAV_USER} apache2-foreground 
       ;;
