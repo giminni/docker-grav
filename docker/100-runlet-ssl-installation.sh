2ea653564b228b0dce67c8c6957ec713ffd0186f
Giovanni Minniti <minniti@t-online.de>
2020-12-27 21:46:03 +0100

    Replace dehydrated script with getssl

--- docker/100-runlet-ssl-installation.sh
+++ docker/100-runlet-ssl-installation.sh
@@ -4,12 +4,11 @@ declare STAGE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${STAGE_DIR}/runlet-head.sh"
 # DO NOT REMOVE ANYTHING BEFORE THIS LINE!
 
-echo "Downloading dehydrated ${DEHYDRATED_VER} package..."
-test -f /tmp/${DEHYDRATED_FILE} || wget -q --show-progress --progress=bar:force \
-     -O /tmp/dehydrated.tgz \
-${DEHYDRATED_URL}/v${DEHYDRATED_VER}/dehydrated-${DEHYDRATED_VER}.tar.gz
-tar -xvzf /tmp/dehydrated.tgz -C /tmp
-cp /tmp/dehydrated-${DEHYDRATED_VER}/dehydrated /usr/local/bin
+echo "Downloading getssl package..."
+test -f /tmp/getssl || wget -q --show-progress --progress=bar:force \
+     -O /usr/local/bin/getssl \
+https://raw.githubusercontent.com/srvrco/getssl/master/getssl
+chmod +x /usr/local/bin/getssl
 
 # DO NOT REMOVE ANYTHING BELOW THIS LINE!
 source "${STAGE_DIR}/runlet-tail.sh"
