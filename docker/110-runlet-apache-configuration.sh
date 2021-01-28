a966901f50a4ee3ecd376d34c9e7d4f55fda4275
Giovanni Minniti <minniti@de.ibm.com>
2021-01-28 13:29:35 +0100

    Process sed under MacOS

--- docker/110-runlet-apache-configuration.sh
+++ docker/110-runlet-apache-configuration.sh
@@ -6,8 +6,13 @@ source "${STAGE_DIR}/runlet-head.sh"
 
 echo "Setting apache modules and rewrites..."
 a2enmod rewrite expires
-sed -i 's/ServerTokens OS/ServerTokens ProductOnly/g' \
+if [ "${OSTYPE:0:6}" == "darwin" ]; then
+   sed -i '' 's/ServerTokens OS/ServerTokens ProductOnly/g' \
         /etc/apache2/conf-available/security.conf
+else
+   sed -i 's/ServerTokens OS/ServerTokens ProductOnly/g' \
+        /etc/apache2/conf-available/security.conf
+fi
 
 # DO NOT REMOVE ANYTHING BELOW THIS LINE!
 source "${STAGE_DIR}/runlet-tail.sh"
