95226164c2ab28fdc9ccf992e0e0d641fff870b0
Giovanni Minniti <minniti@de.ibm.com>
2021-03-15 13:45:50 +0100

    Add move to source directory

--- bin/grav-relink.sh
+++ bin/grav-relink.sh
@@ -1,30 +1,37 @@
-#!/usr/bin/env bash
-RC=0
-
-SOURCE_ARG="${1:-""}"
-TARGET_ARG="${2:-""}"
-
-ROOT_DIR="/var/www/html"
-SOURCE_DIR="../../../pages"
-TARGET_DIR="user/images/g5_anacron"
-
-SOURCE_PATH="${ROOT_DIR}/${TARGET_DIR}/${TARGET_ARG}/${SOURCE_DIR}/${SOURCE_ARG}.${TARGET_ARG}/images"
-TARGET_PATH="${ROOT_DIR}/${TARGET_DIR}/${TARGET_ARG}"
-
-if [ -z "${SOURCE_ARG}" ]; then
-        echo "FAIL: Not enough arguments entered!"
-        echo "INFO: ${0} <page-index> <page-name>"
-        echo "HELP: ${0} 01 home"
-        RC=1
-fi
-
-if [ ${RC} -eq 0 ]; then
-        find "${SOURCE_PATH}" -type f -name "*" -print0 | while read -d $'\0' FILE; do
-                FILE_NAME="${FILE##*/}"
-                cd "${TARGET_PATH}"
-                if [ -e "${FILE_NAME}" ]; then rm "${FILE_NAME}"; fi
-                ln -s ${SOURCE_DIR}/${SOURCE_ARG}.${TARGET_ARG}/images/"${FILE_NAME}" "${FILE_NAME}"
-        done
-fi
-
+#!/usr/bin/env bash                                                                                                                                                                                               
+RC=0                                                                                                                                                                                                              
+                                                                                                                                                                                                                  
+SOURCE_ARG="${1:-""}"                                                                                                                                                                                             
+TARGET_ARG="${2:-""}"                                                                                                                                                                                             
+                                                                                                                                                                                                                  
+ROOT_DIR="/var/www/html"                                                                                                                                                                                          
+SOURCE_DIR="../../../pages"                                                                                                                                                                                       
+TARGET_DIR="user/images/g5_anacron"                                                                                                                                                                               
+                                                                                                                                                                                                                  
+SOURCE_PATH="${ROOT_DIR}/${TARGET_DIR}/${TARGET_ARG}/${SOURCE_DIR}/${SOURCE_ARG}.${TARGET_ARG}/images"                                                                                                            
+TARGET_PATH="${ROOT_DIR}/${TARGET_DIR}/${TARGET_ARG}"                                                                                                                                                             
+                                                                                                                                                                                                                  
+if [ -z "${SOURCE_ARG}" ]; then                                                                                                                                                                                   
+        echo "FAIL: Not enough arguments entered!"                                                                                                                                                                
+        echo "INFO: ${0} <page-index> <page-name>"                                                                                                                                                                
+        echo "HELP: ${0} 01 home"                                                                                                                                                                                 
+        RC=1                                                                                                                                                                                                      
+fi                                                                                                                                                                                                                
+                                                                                                                                                                                                                  
+cd "${TARGET_PATH}"                                                                                                                                                                                               
+                                                                                                                                                                                                                  
+if [ ${RC} -eq 0 ]; then                                                                                                                                                                                          
+        find "${TARGET_PATH}" -type f -name "*" -print0 | while read -d $'\0' FILE; do                                                                                                                            
+                FILE_NAME="${FILE##*/}"                                                                                                                                                                           
+                                                                                                                                                                                                                  
+                # If file does not exist move it to source directory                                                                                                                                              
+                if [ ! -e "${SOURCE_PATH}/${FILE_NAME}" ]; then                                                                                                                                                   
+                        mv "${FILE_NAME}" "${SOURCE_DIR}/${SOURCE_ARG}.${TARGET_ARG}/images"                                                                                                                      
+                fi                                                                                                                                                                                                
+                                                                                                                                                                                                                  
+                ln -s "${SOURCE_DIR}/${SOURCE_ARG}.${TARGET_ARG}/images/${FILE_NAME}" "${FILE_NAME}"                                                                                                              
+                chmod 660 "${SOURCE_DIR}/${SOURCE_ARG}.${TARGET_ARG}/images/${FILE_NAME}"                                                                                                                         
+        done                                                                                                                                                                                                      
+fi                                                                                                                                                                                                                
+                                                                                                                                                                                                                  
 exit ${RC}
